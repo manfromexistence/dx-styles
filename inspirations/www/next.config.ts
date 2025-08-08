e478daa3a62142657beca42e3dff8e3efe423c72
robinjusticeleague <robinjusticeleague@gmail.com>
2025-08-08 11:39:34 +0000

    feat: postcss vs dx-file watching

--- inspirations/www/next.config.ts
+++ inspirations/www/next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', "3000-firebase-dx-1754484811905.cluster-6dx7corvpngoivimwvvljgokdw.cloudworkstations.dev"],
 }
 
 export default nextConfig
