33c5d8d75b22e719420af153b3db8ea3081c8ec7
robinjusticeleague <robinjusticeleague@gmail.com>
2025-08-10 19:36:37 +0000

    feat: dx styles

--- playgrounds/nextjs/app/page.tsx
+++ playgrounds/nextjs/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex items-start justify-start">
+    <div className="flex items-center justify-center">
       <span className="text-5xl font-bold">Dx Styles</span>
     </div>
   );
