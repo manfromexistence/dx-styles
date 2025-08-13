4e71c629f694538550dc1f94296b12a12824673b
robinjusticeleague <robinjusticeleague@gmail.com>
2025-08-13 14:20:20 +0000

    feat: css step is very slow

--- playgrounds/nextjs/app/page.tsx
+++ playgrounds/nextjs/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="grid p-5 h-screen w-full place-content-start place-items-start items-start justify-start absolute">
+    <div className="grid p-4 h-screen w-full place-content-start place-items-start items-start justify-start absolute">
       <span className="text-5xl font-bold">Dx Styles</span>
     </div>
   );
