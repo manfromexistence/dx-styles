d39e84ee5f7675892495c842d545455b287797a0
robinjusticeleague <robinjusticeleague@gmail.com>
2025-08-14 12:03:49 +0000

    warning: things can break

--- playgrounds/nextjs/app/page.tsx
+++ playgrounds/nextjs/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="md:flex flex-col p-8 h-screen w-full place-content-start place-items-start items-start justify-start absolute">
+    <div className="md:flex flex-col md:p-5 h-screen w-full place-content-start place-items-start items-start justify-start absolute">
       <span className="text-5xl font-bold">Dx Styles</span>
     </div>
   );
